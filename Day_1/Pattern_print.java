099e4a0019c855a6c5ce2b1a4e87013870534304
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2022-10-14 18:34:50 +0530

    Initial Commit

---
+++ Day_1/Pattern_print.java
@@ -0,0 +1,29 @@
+package Day_1;
+
+import java.util.*;
+/*
+ *        ***********
+ *        *         *
+ *        *         *
+ *        *         *
+ *        ***********
+ * 
+ */
+
+public class Pattern_print {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int coloum = sc.nextInt();
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= coloum; j++) {
+                if (i == 1 || i == row || j == 1 || j == coloum) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
