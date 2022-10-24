1a860565ec0f63cd7129391ce89869ff7256e589
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2022-10-24 15:51:00 +0530

    Initial commit

---
+++ Day_8/Check_Same_tree.java
@@ -0,0 +1,31 @@
+package Day_8;
+
+/*
+Given two root nodes of two separate binary trees, check whether the trees are identical.
+
+Two binary trees are considered the same if they have identical shapes, and the corresponding nodes have the same value.
+ */
+public class Check_Same_tree {
+
+    public static void areTreesIdentical(Node root1, Node root2) {
+        if (helper(root1, root2)) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
+        }
+    }
+
+    public static boolean helper(Node a, Node b) {
+        if (a == null && b == null)
+            return true;
+
+        /* 2. both non-empty -> compare them */
+        if (a != null && b != null)
+            return (a.data == b.data
+                    && helper(a.left, b.left)
+                    && helper(a.right, b.right));
+
+        /* 3. one empty, one not -> false */
+        return false;
+    }
+}
