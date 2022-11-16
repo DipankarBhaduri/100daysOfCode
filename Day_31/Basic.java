b94fee2b280e572e16a53b3d3d5dbfd9aa3f3a1f
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2022-11-16 20:34:15 +0530

    basic

---
+++ Day_31/Basic.java
@@ -0,0 +1,23 @@
+package Day_31;
+
+import java.util.*;
+
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 5;
+        float num2 = sc.nextFloat();
+        float num3 = sc.nextFloat();
+
+        float mul = num1 * num3 * num2;
+        float add = num1 + num2 + num3;
+        System.out.println(mul);
+        System.out.println(add);
+
+        float ans = mul / add;
+        System.out.printf("%.2f", ans);
+        System.out.println();
+        System.out.print(mul / add);
+
+    }
+}
